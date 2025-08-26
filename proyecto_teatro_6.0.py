a2163ef14ede319bf0a3df1a92d71af3a01f6f0f
gumeyer <gumeyer@uade.edu.ar>
2025-08-25 21:43:30 -0300

    Add files via upload
    esta mejor modulado con menos lineas y falta hacer bien el registro

---
+++ proyecto_teatro_6.0.py
@@ -0,0 +1,255 @@
+from nombres_teatro import *
+from funciones_de_teatro import *
+
+
+inicio=True
+admin=False
+
+#region ingreso
+#es para empezar el ingreso
+while inicio==True:
+    #es la funcion que dicta que puede poner el usuario para cada opcion
+    ingreso=int(input("Si posse un usuario presione 0 para ingresar:\nSi no posee presione 1 para crear un usuario nuevo:"))
+    #validacion de que toque 0 o 1 exclusivamente
+    while ingreso !=0 and ingreso !=1:
+        print("ingreso no valido seleccione 1 o 0 ")
+        ingreso=int(input("Si posse un usuario presione 0 para ingresar:\nSi no posee presione 1 para crear un usuario nuevo:"))
+
+    #inicio de el ingreso de un usuario creado anteriormente
+    if ingreso==0:
+        #ingreso del dni con validacion de que exista en los datos globales de dnis (cambiar lista por una lista paralela)
+        dni=int(input("ingrese su dni:"))
+        if dni not in datos_globales_dni:
+            print("el dni ingresado no existe")
+            dni=int(input("ingrese su dni:"))
+        #ingreso de la contraseña con validacion de que exista en los datos globales de dnis (cambiar lista por una lista paralela)
+        contraseña=input("ingrese su contraseña: ")
+        while contraseña not in datos_globales_contraseñas:
+            print("contraseña incorrecta")
+            contraseña=input("ingrese su contraseña:")
+        #esto es para que alguien sea admin necesitamos que en lugar de 
+        # cambiar uno por uno hacer un lista con dnis y contraseñas que sean para admin    
+        if (dni==47346945 or dni==46624535) and (contraseña=="superadmin"):
+            #esto hace que puedas desbloquear opciones de admin
+            admin=True
+            #esto es para avanzar al siguiente  paso que es el de opciones
+            menu=True
+        #esto es una validacion extra que no es  exactamente necesaria porque las 
+        # hacen antes pero es para validar que el dni y la contraseaña sean correctas 
+        # (deberia validar que sea el dni y la contraseña de la misma contraseña)
+        elif dni in datos_globales_dni and contraseña in datos_globales_contraseñas:
+            menu=True
+
+        inicio=False
+
+    elif ingreso==1:
+    #CREAR USUARIO
+        #llama a una funcion para asignar un id a cada usuario
+        id_usuario = id_user()
+        #es para ingresar un nombre que tenga un maximo de 8 caracteres mas de eso se rompe la matriz
+        #hace una validacion basica de que no puede exceder los 8 caracteres o modifica de 8 a mas caracteres con ese 8 de abajo
+        nombre = input("ingrese nombre:")
+        nombre=nombre.ljust(8, " ")
+        #esto es para ingresar un dni con la validacion de que no puede estar anteriormente en la lista de datos globales
+        dni = int(input("ingrese documento: "))
+        while dni not in datos_globales_dni:
+            datos_globales_dni.append(dni)     
+        #se ingresa el telefono con la validacion de que sea mayor a 1100000000 o menor a 1199999999
+        telefono = int(input("ingrese numero de telefono: "))
+        while telefono<1100000000 or telefono>1199999999:
+            print("el numero colocado no es valido")
+            telefono = int(input("ingrese numero de telefono: "))
+        # se ingresa el correo y si no tiene gmail yahoo hotmail te rebota y te manda a hacerlo de vuelta
+        correo = input("ingrese correo ")
+        while "@gmail.com" not in correo and "@yahoo.com" not in correo and "@hotmail.com" not in correo:
+            print("su correo debe tener @gmail, @yahoo o @hotmail")
+            correo = input("ingrese correo")
+        #se ingresa una contraseña que tenga mas de 5 caracteres y si es menos rebota rebota
+        contraseña= input("ingrese una contraseña de mas de  5 caracteres")
+        while len (contraseña) <5:  
+            print("contraseña invalida tiene que tener mas de 5 caracteres")
+            contraseña= input("ingrese una contraseña ")
+        if len(contraseña) >5:
+            print("contraseña creada con exito")
+            datos_globales_contraseñas.append(contraseña)
+        #si todas las validaciones funcionan agrega todos estos datos y le agrega un dato que es True que siempre tiene que estar
+        #porque es para ver si esta activo o inactivo
+        datos_globales_usuarios.append([id_usuario,nombre,dni,telefono,correo,True])  
+        menu=True
+#region program princ
+#PROGRAMA PRINCIPAL
+while menu==True:
+    if admin==True:
+        print("\n1-MOSTRAR SHOWS\n2-GENERAR RESERVAS\n3-BUSCAR SHOWS\n4-CERRAR SESION\n5-SALIR\n6-RESERVAS\n7-GENERAR SHOW\n8-VER USUARIOS\n9-BORRAR USUARIO\n10-BORRAR RESERVAS\n11-BORRAR SHOW")
+        usuario = int(input("Elige una opcion: "))
+    if admin==False:
+        print("\n1-MOSTRAR SHOWS\n2-GENERAR RESERVA\n3-BUSCAR SHOW\n4-CERRAR SESION\n5-SALIR")
+        usuario = int(input("Elige una opcion: "))
+    #region opciones
+    if usuario == 1: #MOSTRAR SHOWS
+       matriz_ordenada = sorted(datos_globales, key=lambda x: x[5])
+       ver_m(matriz_ordenada) 
+    elif usuario == 2: #GENERAR RESERVA
+        id_reserva = id_alt_r()  # Llamar a la función
+
+        id_usuario = int(input("ingresar el numero de id"))
+        while id_usuario not in solo_ids_usuario:
+            print("Id inexistente")
+
+            id_usuario = int(input("ingresar el numero de id"))
+        print("-----------------")
+        print("para platea elija 1")
+        print("para campo elija 2")
+        print("para vip elija 3")
+        print("-----------------")
+
+        ubicacion_u = int(input("Elegi tipo de ubiacion: "))
+        while ubicacion_u >3 or ubicacion_u <=0:
+            print("no es valido el numero")
+            ubicacion_u = int(input("Elegi tipo de ubiacion: "))
+        if ubicacion_u == 1:
+            ubicacion_e = "platea   "
+        elif ubicacion_u == 2:
+            ubicacion_e = "campo    "
+        elif ubicacion_u == 3:
+            ubicacion_e = "vip       "
+
+        ver_m(matriz) 
+        show = int(input("ingrese el numero de id del show que desea asistir: "))
+        while show not in solo_ids_show:
+            print("El id ingresado no existe")
+            show = int(input("porfavor,ingrese el numero de id del show que desea asistir: "))
+        for i in datos_globales:
+            if i[0] == show:
+                i[4] = i[4] - 1
+        datos_globales_reserva.append([id_reserva, id_usuario, ubicacion_e, show])
+        print("AGREGADO CORRECTAMENTE!")
+    elif usuario == 3: #BUSCAR SHOWS
+
+        print("\n1-BUSCAR POR ID\n2-BUSCAR POR FECHA")
+
+        elec = int(input(" "))
+
+        if elec == 1:
+
+            elec = int(input("inrese id"))
+
+            lista_temp = []
+
+            for i in datos_globales:
+                if i[0] == elec:
+                    lista_temp.append(i)
+
+            if len(lista_temp) > 0:
+                ver_m(lista_temp) 
+            else:
+                print("no coincide con ningun id")
+
+        elif elec == 2:
+        
+            año = int(input("ingrese año"))
+            mes = int(input("ingrese mes"))
+            dia = int(input("ingrese dia: "))
+            fecha_buscada = datetime(año, mes, dia).date()
+
+            lista_temp = []
+
+            for i in datos_globales:
+                if i[5] == fecha_buscada:
+                    lista_temp.append(i)
+
+
+            if len(lista_temp) > 0:
+                ver_m(lista_temp) 
+            else:
+                print("no hay fechas disponibles")
+    elif usuario == 4: #SALIR DE LA SESION
+        game=True
+        admin=False
+    elif usuario==5:
+        break
+
+    elif usuario == 6 and admin==True: #MOSTRAR RESERVAS
+       ver_m2(matriz2)
+
+    elif usuario == 7 and admin==True: #GENERAR SHOWS
+        id_show = id_alt()
+
+        tipo_Evento = input("Ingrese el tipo de evento")
+        tipo_Evento=tipo_Evento.ljust(20, " ")
+
+        duracion = int(input("ingrese la duracion del evento"))
+
+        espectadores = int(input("ingrese la capacidad maxima de espectadores"))
+
+        espacios_disponibles = random.randint(0,20)
+
+
+        año = int(input("ingrese año"))
+        mes = int(input("ingrese mes"))
+        dia = int(input("ingrese dia: "))
+        fecha = datetime(año, mes, dia).date()
+
+        #comprobar si se pasan los minuts
+
+        lista_temp = []
+
+        for i in datos_globales:
+            if i[5] == fecha:
+                lista_temp.append(i)
+
+
+        suma = 0
+        columna = 2
+        for f in lista_temp:
+            suma += f[columna]
+        
+        if (suma + duracion) < 720:
+
+            datos_globales.append([id_show,tipo_Evento,duracion,espectadores,espacios_disponibles,fecha])
+        else:
+            print("No hay espacio en el dia para el show ingresado")
+
+
+    elif usuario == 8 and admin==True: #VER USUARIOS
+        
+        ver_m3(matriz3)
+
+   
+    elif usuario == 9 and admin==True: #BORRAR USUARIO
+
+        eleccion = int(input("Seleccione id a eliminar "))
+
+        for i in datos_globales_usuarios:
+            if i[0] == eleccion:
+                i[5] = False
+
+        for i in datos_globales_reserva[:]:
+            if i[1] == eleccion:
+                datos_globales_reserva.remove(i)
+    elif usuario == 10 and admin==True: #BORRAR RESERVA
+
+        eleccion = int(input("Seleccione id de reserva a eliminar "))
+
+
+        for i in datos_globales_reserva[:]:
+            if i[0] == eleccion:
+                datos_globales_reserva.remove(i)
+
+    elif usuario == 11 and admin==True: #BORRAR SHOW
+        eleccion = int(input("Ingrese id del show: "))
+
+        # Borrar el show
+        for s in datos_globales[:]:
+            if s[0] == eleccion:
+                datos_globales.remove(s)
+
+        # Borrar todas las reservas asociadas
+        for r in datos_globales_reserva[:]:
+            if r[3] == eleccion:
+                datos_globales_reserva.remove(r)
+
+        # Actualizar lista de ids de shows
+        solo_ids_show.clear()
+        for s in datos_globales:
+            solo_ids_show.append(s[0])
